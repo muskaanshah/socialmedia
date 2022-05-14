0073fd6c41fa517bff8a70ed1d5d4839c7a0fb66
muskaanshah <hmuskaan11@gmail.com>
2022-05-15 01:22:19 +0530

    feat: navlink added

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import {
   Avatar,
   Box,
@@ -5,6 +6,7 @@ import {
   Divider,
   HStack,
   Image,
+  Link,
   Text,
   VStack,
   useColorModeValue,
@@ -28,28 +30,67 @@ function Drawer() {
     >
       <VStack justifyContent="space-between" w="full" py={4}>
         <VStack align="flex-start" spacing={6} w="full" pr="8">
-          <HStack>
-            <Image boxSize="50px" objectFit="cover" src={logo} alt="Medioso" />
-            <Text fontSize="3xl" textStyle="logo" color={logoColor}>
-              Medioso
-            </Text>
-          </HStack>
-          <HStack>
-            <span className="material-icons-outlined">home</span>
-            <Text fontSize="1rem">Home</Text>
-          </HStack>
-          <HStack>
-            <span className="material-icons-outlined">explore</span>
-            <Text fontSize="1rem">Explore</Text>
-          </HStack>
-          <HStack>
-            <span className="material-icons-outlined">bookmark_border</span>
-            <Text fontSize="1rem">Saved</Text>
-          </HStack>
-          <HStack>
-            <span className="material-icons-outlined">notifications</span>
-            <Text fontSize="1rem">Notifications</Text>
-          </HStack>
+          <NavLink to="/home">
+            <HStack>
+              <Image
+                boxSize="50px"
+                objectFit="cover"
+                src={logo}
+                alt="Medioso"
+              />
+              <Text fontSize="3xl" textStyle="logo" color={logoColor}>
+                Medioso
+              </Text>
+            </HStack>
+          </NavLink>
+          <Link
+            as={NavLink}
+            to="/home"
+            _hover={{ color: logoColor, textDecoration: 'none' }}
+            _focus={{ outline: 'none' }}
+            _activeLink={{ color: logoColor, fontWeight: 'bold' }}
+          >
+            <HStack>
+              <span className="material-icons-outlined">home</span>
+              <Text fontSize="1rem">Home</Text>
+            </HStack>
+          </Link>
+          <Link
+            as={NavLink}
+            to="/explore"
+            _hover={{ color: logoColor, textDecoration: 'none' }}
+            _focus={{ outline: 'none' }}
+            _activeLink={{ color: logoColor, fontWeight: 'bold' }}
+          >
+            <HStack>
+              <span className="material-icons-outlined">explore</span>
+              <Text fontSize="1rem">Explore</Text>
+            </HStack>
+          </Link>
+          <Link
+            as={NavLink}
+            to="/saved"
+            _hover={{ color: logoColor, textDecoration: 'none' }}
+            _focus={{ outline: 'none' }}
+            _activeLink={{ color: logoColor, fontWeight: 'bold' }}
+          >
+            <HStack>
+              <span className="material-icons-outlined">bookmark_border</span>
+              <Text fontSize="1rem">Saved</Text>
+            </HStack>
+          </Link>
+          <Link
+            as={NavLink}
+            to="/notifications"
+            _hover={{ color: logoColor, textDecoration: 'none' }}
+            _focus={{ outline: 'none' }}
+            _activeLink={{ color: logoColor, fontWeight: 'bold' }}
+          >
+            <HStack>
+              <span className="material-icons-outlined">notifications</span>
+              <Text fontSize="1rem">Notifications</Text>
+            </HStack>
+          </Link>
           <Button
             borderRadius="2xl"
             w="full"
@@ -59,19 +100,27 @@ function Drawer() {
             Add a post
           </Button>
         </VStack>
-        <HStack spacing={3} w="full">
-          <Avatar
-            name="Dan Abrahmov"
-            src="https://bit.ly/dan-abramov"
-            size="md"
-          />
-          <VStack align="flex-start">
-            <Text fontSize="1rem">Profile</Text>
-            <Text fontSize="1rem" className="mt-0" color="gray.500">
-              Username
-            </Text>
-          </VStack>
-        </HStack>
+        <Link
+          as={NavLink}
+          to="/profile"
+          _hover={{ textDecoration: 'none' }}
+          _focus={{ outline: 'none' }}
+          w="full"
+        >
+          <HStack spacing={3} w="full">
+            <Avatar
+              name="Dan Abrahmov"
+              src="https://bit.ly/dan-abramov"
+              size="md"
+            />
+            <VStack align="flex-start">
+              <Text fontSize="1rem">Profile</Text>
+              <Text fontSize="1rem" className="mt-0" color="gray.500">
+                Username
+              </Text>
+            </VStack>
+          </HStack>
+        </Link>
       </VStack>
       <Divider orientation="vertical" />
       <AddPostModal isOpen={isOpen} onClose={onClose} />
