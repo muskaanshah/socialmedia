7ecce45196799cf0b7775553a1ea64d1a9f144ea
muskaanshah <hmuskaan11@gmail.com>
2022-05-20 19:28:05 +0530

    style: loader on going to auth pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes as RoutesContainer } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Center, Spinner } from '@chakra-ui/react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 import App from './App';
@@ -21,9 +22,11 @@ import { setCurrentUserData } from './pages/authentication/authSlice';
 import { PrivateRoute } from './utils';
 
 function Routes() {
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async user => {
+      setIsLoading(true);
       if (user) {
         const userObj = await getDoc(doc(db, `users/${user.uid}`));
         const data = userObj.data();
@@ -31,6 +34,7 @@ function Routes() {
       } else {
         dispatch(setCurrentUserData(null));
       }
+      setIsLoading(false);
     });
     return () => {
       unsubscribe();
@@ -38,23 +42,29 @@ function Routes() {
   }, [dispatch]);
   return (
     <>
-      <RoutesContainer>
-        <Route element={<PrivateRoute />}>
-          <Route element={<App />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/saved" element={<Saved />} />
-            {/* <Route path="/notifications" element={<Notifications />} /> */}
-            <Route path="/profile/:userID" element={<Profile />} />
-            <Route path="/post/:postID" element={<SinglePost />} />
+      {isLoading ? (
+        <Center minH="100vh">
+          <Spinner size="xl" />
+        </Center>
+      ) : (
+        <RoutesContainer>
+          <Route element={<PrivateRoute />}>
+            <Route element={<App />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/saved" element={<Saved />} />
+              {/* <Route path="/notifications" element={<Notifications />} /> */}
+              <Route path="/profile/:userID" element={<Profile />} />
+              <Route path="/post/:postID" element={<SinglePost />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-        <Route element={<PrivateRoute authRoute />}>
-          <Route path="/" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-        </Route>
-      </RoutesContainer>
+          <Route path="*" element={<NotFound />} />
+          <Route element={<PrivateRoute authRoute />}>
+            <Route path="/" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
+        </RoutesContainer>
+      )}
       <ToastContainer
         position="bottom-right"
         autoClose={3000}
