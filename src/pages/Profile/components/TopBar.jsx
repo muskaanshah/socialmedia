84482c080f023898a750722a11f99590580e2ffe
muskaanshah <hmuskaan11@gmail.com>
2022-05-20 17:11:22 +0530

    fix: topbar

--- src/pages/Profile/components/TopBar.jsx
+++ src/pages/Profile/components/TopBar.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import {
   Box,
   Button,
@@ -12,14 +13,15 @@ import { TopBarStyles } from '../../../styles/globalStyles';
 
 function TopBar() {
   const bgCol = useColorModeValue('blackAlpha.200', 'blackAlpha.500');
+  const { singleUser } = useSelector(state => state.user);
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box bgColor={bgCol} sx={TopBarStyles}>
       <HStack w="full" justify="space-between">
         <VStack spacing={0} align="flex-start">
-          <Text fontSize="lg">Muskaan Shah</Text>
+          <Text fontSize="lg">{singleUser?.name}</Text>
           <Text fontSize="sm" color="gray.500">
-            39 posts
+            {singleUser?.posts?.length} posts
           </Text>
         </VStack>
         <Button
